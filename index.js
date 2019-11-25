7b0e581bc0a14091a05c101140c83415dbbf122b
rohith-s <rohith.s@congruentglobal.com>
2019-11-25 18:43:53 +0530

    Renamed server_port to port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 console.log('port:',port);
 console.log('server_ip_address:',server_ip_address);
 server.listen(port, server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", port " + server_port )
+    console.log( "Listening on " + server_ip_address + ", port " + port )
 });
 server.on('error', onError);
 server.on('listening', onListening);
